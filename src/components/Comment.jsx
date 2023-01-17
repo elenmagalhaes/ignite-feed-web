a8b7836ac7e3a4289a64616418e0eaf319fe4767
Elen Magalhães <fromeleen@gmail.com>
2023-01-17 18:40:03 -0300

    componetizando os avatares da aplicação

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,14 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment() {
     return (
         <div className={styles.comment}>
-            <img src="https://github.com/elenmagalhaes.png" />
+            <Avatar
+                hasBorder={false}
+                src="https://github.com/elenmagalhaes.png"
+            />
 
             <div className={styles.commentContent}>
                 <div className={styles.commentBox}>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -10,9 +11,9 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img
-                    className={styles.avatar} 
-                    src="https://avatars.githubusercontent.com/u/51779470?v=4" />
+                <Avatar
+                    src="https://avatars.githubusercontent.com/u/51779470?v=4"
+                />
                 <strong>Elen Magalhães</strong>
                 <span>Web delevoper</span>
             </div>
