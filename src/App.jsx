fb57cbb402f861e1772bb46e116f16503c3d8535
Elen Magalhães <fromeleen@gmail.com>
2023-01-19 00:35:14 -0300

    adicionando feature para deletar comentários

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,69 @@
-import { Post } from './components/Post';
-import { Header } from './components/Header';
-import { Sidebar } from './components/Sidebar';
+import { Post } from './components/Post'
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 
-import styles from './App.module.css';
-import './global.css';
+import styles from './App.module.css'
+import './global.css'
 
 const posts = [
   {
-      id: 1,
-      author: {
-          avatarUrl: 'https://github.com/elenmagalhaes.png',
-          name: 'Elen Magalhães',
-          role: 'Software Engineer III'
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/elenmagalhaes.png',
+      name: 'Elen Magalhães',
+      role: 'Software Engineer III',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
       },
-      content: [
-          { type: 'paragraph', content: 'Fala galeraa 👋' },
-          { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
-          { type: 'link', content: 'jane.design/doctorcare' }
-      ],
-      publishedAt: new Date('2023-01-17 20:00:00')
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-01-17 20:00:00'),
   },
   {
-      id: 2,
-      author: {
-          avatarUrl: 'https://github.com/diego3g.png',
-          name: 'Diego Fernandes',
-          role: 'CEO @Rocketseat'
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CEO @Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
       },
-      content: [
-          { type: 'paragraph', content: 'Fala galeraa 👋' },
-          { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
-          { type: 'link', content: 'jane.design/doctorcare' }
-      ],
-      publishedAt: new Date('2023-01-10 15:00:00')
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-01-10 15:00:00'),
   },
-];
+]
 
 function App() {
-
   return (
-   <div>
-    <Header />
-    <div className={styles.wrapper}>
-      <Sidebar />
-      <main>
-        { 
-          posts.map(post => {
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
             return (
               <Post
+                key={post.id}
                 author={post.author}
                 content={post.content}
                 publishedAt={post.publishedAt}
               />
             )
-          })
-        }
-      </main>
+          })}
+        </main>
+      </div>
     </div>
-   </div>
   )
 }
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,6 +33,14 @@ export function Post({ author, content, publishedAt }) {
     setNewCommentText(event.target.value)
   }
 
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter((comment) => {
+      return comment !== commentToDelete
+    })
+
+    setComments(commentsWithoutDeletedOne)
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -55,10 +63,10 @@ export function Post({ author, content, publishedAt }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>
           } else if (line.type === 'link') {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="">{line.content}</a>
               </p>
             )
@@ -81,7 +89,13 @@ export function Post({ author, content, publishedAt }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment content={comment} />
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          )
         })}
       </div>
     </article>
